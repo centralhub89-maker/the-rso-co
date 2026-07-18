f82ece6b83f6406c047eecbeff0f7c6d2a5d1944
centralhub89-maker <centralhub89@gmail.com>
2026-07-19 03:06:28 +0700

    Add components index file for easier imports

---
+++ components/index.ts
@@ -0,0 +1,16 @@
+// Layout
+export { Layout, default as LayoutComponent } from './Layout';
+export { Header, default as HeaderComponent } from './Header';
+export { Footer, default as FooterComponent } from './Footer';
+
+// Sections
+export { HeroSection, default as HeroSectionComponent } from './HeroSection';
+export { FeaturesSection, default as FeaturesSectionComponent } from './FeaturesSection';
+export { ProductGrid, default as ProductGridComponent } from './ProductGrid';
+
+// Cards
+export { ProductCard, type Product } from './ProductCard';
+export { FeatureCard, default as FeatureCardComponent } from './FeatureCard';
+
+// UI Components
+export { Button, LinkButton, default as ButtonComponent } from './Button';
